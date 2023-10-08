2a5e7f2004514961b518c125495db4a68ea229d9
Alexandr <megalistnn@gmail.com>
2023-10-08 15:10:11 +0300

    feat: add a condition for the function to work correctly

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
-function check(str, bracketsConfig) {
+module.exports = function check(str, bracketsConfig) {
 	const arr = str.split('')
 	const openBrackets = bracketsConfig.map(item => item[0]);
 	const closeBrackets = bracketsConfig.map(item => item[1]);
 	const stack = [];
+
 	if (arr.length % 2 !== 0) { return false }
+
 	for (let i = 0; i < arr.length; i++) {
+
+		if (openBrackets.includes(arr[i]) && closeBrackets.indexOf(arr[i]) > -1 && stack[stack.length - 1] === arr[i]) {
+			stack.pop();
+			continue;
+		}
+
 		if (openBrackets.includes(arr[i])) {
 			stack.push(arr[i]);
+			continue;
 		}
+
 		if (closeBrackets.indexOf(arr[i]) > -1) {
-			const index = closeBrackets.indexOf(arr[i])
+			const index = closeBrackets.indexOf(arr[i]);
+
 			if (openBrackets.indexOf(stack[stack.length - 1]) === index) {
 				stack.pop();
 			}
 		}
 		if (!openBrackets.includes(arr[i]) && closeBrackets.indexOf(arr[i]) < 0) {
 			return false;
-
 		}
 	}
 
